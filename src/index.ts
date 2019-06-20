99d123c8b0a5c01b60f261b1c29d3392b26e9d93
Deepak Mittal <dpac.mittal2@gmail.com>
2019-06-20 08:33:22 +0530

    Created a very simple router

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import React, { useState, createContext, useContext, useEffect } from "react";
+
+const RouteContext = createContext({ currentUrl: "/", setCurrentUrl: null });
+
+const stylesheet = {
+  bordered: {
+    border: {
+      type: "line"
+    },
+    style: {
+      border: {
+        fg: "blue"
+      }
+    }
+  },
+  magentaBackground: {
+    style: {
+      bg: "magenta"
+    }
+  }
+};
+export const Route = ({ children, path, ...props }) => {
+  const { currentUrl } = useContext(RouteContext);
+  //console.log("Current url is ", currentUrl);
+  if (currentUrl == path)
+    return <blessed-box {...props}>{children}</blessed-box>;
+  return null;
+};
+
+export const Link = ({ to, children }) => {
+  const { currentUrl, setCurrentUrl } = useContext(RouteContext);
+  function switchRoute() {
+    if (to == currentUrl) return null;
+    //console.log("Setting current url to ", to);
+    setCurrentUrl(to);
+  }
+  return (
+    <blessed-button
+      class={stylesheet.bordered}
+      shrink={true}
+      mouse={true}
+      onPress={switchRoute}
+      onClick={switchRoute}
+      clickable={true}
+    >
+      {children}
+    </blessed-button>
+  );
+};
+
+const SimpleBlessedRouter = props => {
+  const [currentUrl, setCurrentUrl] = useState(props.defaultUrl || "/");
+  return (
+    <RouteContext.Provider value={{ currentUrl, setCurrentUrl }}>
+      {props.children}
+    </RouteContext.Provider>
+  );
+};
+
+export default SimpleBlessedRouter;
